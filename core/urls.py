cb8b9688f9908faec97b1a0e60de380595de1266
SteppenHorde <steppenhorde@yandex.ru>
2019-06-04 13:15:15 +0300

    initial

---
+++ core/urls.py
@@ -0,0 +1,30 @@
+"""sh_simple_IO_apps URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
+
+
+
+urlpatterns = [
+    path('', views.Main.as_view(), name='main'),
+    path('accounts/signup/', views.SignUp.as_view(), name='signup'),
+    path('accounts/profile/', views.Profile.as_view(), name='profile'),
+    path('accounts/change_email/', views.ChangeEmail.as_view(), name='change_email'),
+    path('accounts/', include('django.contrib.auth.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ core/views.py
@@ -0,0 +1,61 @@
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import generic, View
+from django.http import HttpResponse
+from django.views.generic.base import TemplateView
+
+from .forms import EmailForm
+
+
+
+class Main(TemplateView):
+    template_name = 'core/main.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(**kwargs)
+        #return render(request, self.template_name, context=context)
+        return context
+
+
+class SignUp(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login') # после регистрации переход на вход
+    template_name = 'registration/signup.html'
+
+
+class Profile(View):
+    template_name = 'user/profile.html'
+
+    def get(self, request, *args, **kwargs):
+        context = {
+
+        }
+
+        return render(request, self.template_name, context=context)
+
+
+class ChangeEmail(View):
+    email_form = EmailForm
+    initial = {'key': 'value'}
+    template_name = 'user/change_email.html'
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class(initial=self.initial)
+        context = {
+            'user': request.user,
+            'form': form,
+        }
+        print(f'EMAIL_FIELD: {request.user.EMAIL_FIELD}')
+        print(f'email: {request.user.email}')
+        print(f'email_user: {request.user.email_user}')
+        print(f'get_email_field_name: {request.user.get_email_field_name}')
+
+        return render(request, self.template_name, context=context)
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            return HttpResponseRedirect('/success/')
+
+        return render(request, self.template_name, {'form': form})
